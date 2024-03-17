339ffe64ffbbbd7f19510f040a1f923e9637b99b
dotPast <83116565+dotPast@users.noreply.github.com>
2024-03-17 16:42:51 +0300

    joke eula for ssmp

---
+++ pages/ssmp-evla.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>End Visitor License Agreement for dotPast's home on Streamers SMP server.</h1>
+  <div class="code">
+    <UIcon class="lang-icon" name="i-skill-icons-htmx-dark" dynamic/> LICENSE
+    <Shiki lang="txt" :code="evla" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+var evla = `1. Visitor must agree to EVLA before entering.
+2. dotPast is not resopnsible for deaths from falling through the floor.
+3. dot can kick you the fuck out whenever he wants to
+4. Visitor is not allowed to break or place blocks without permission.
+5. Visitor is not allowed to take items out of containers without permission.
+6. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. Quod idem licet transferre in voluptatem, ut postea variari voluptas distinguique possit, augeri amplificarique non possit. At etiam Athenis, ut e patre audiebam facete et urbane Stoicos irridente, statua est in quo a nobis philosophia defensa et collaudata est, cum id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum defuturum, quas natura non depravata desiderat. Et quem ad me accedis, saluto: 'chaere,' inquam, 'Tite!' lictores, turma omnis chorusque: 'chaere, Tite!' hinc hostis mi Albucius, hinc inimicus. Sed iure Mucius. Ego autem mirari satis non queo unde hoc sit tam insolens domesticarum rerum fastidium. Non est omnino hic docendi locus; sed ita prorsus existimo, neque eum Torquatum, qui hoc primus cognomen invenerit, aut torquem illum hosti detraxisse, ut aliquam ex eo est consecutus? -- Laudem et caritatem, quae sunt vitae sine metu degendae praesidia firmissima. -- Filium morte multavit. -- Si sine causa, nollem me ab eo delectari, quod ista Platonis, Aristoteli, Theophrasti orationis ornamenta neglexerit. Nam illud quidem physici, credere aliquid esse minimum, quod profecto numquam putavisset, si a Polyaeno, familiari suo, geometrica discere maluisset quam illum etiam ipsum.
+`
+</script>
+
+<style>
+
+</style>
