142e7ed040ca6a999613987a0968d01944818c8c
dotPast <83116565+dotPast@users.noreply.github.com>
2024-07-02 20:08:44 +0300

    rearrange header

--- app.vue
+++ app.vue
@@ -6,13 +6,13 @@
           <NuxtImg src="https://avatars.githubusercontent.com/u/83116565" width="48" class="rounded-xl mr-4"/>
         </NuxtLink>
 
-        <NuxtImg src="https://api.statusbadges.me/badge/status/585025499995045888?simple=true&style=flat-square" class="rounded mr-4"/>
-
         <UButtonGroup>
           <UButton to="/" icon="tabler:link" size="lg" color="black" variant="solid" label="/ links" />
           <UButton to="/about" icon="tabler:user" size="lg" color="black" variant="solid" label="/ about" />
           <UButton to="/what-i-worked-on" icon="tabler:hammer" size="lg" color="black" variant="solid" label="/ what i worked on" />
         </UButtonGroup>
+
+        <NuxtImg src="https://api.statusbadges.me/badge/status/585025499995045888?simple=true&style=flat-square" class="rounded mr-4"/>
       </div>
       <NuxtPage class="mt-2"/>
       <h6 class="mt-4">Status Badge: <NuxtLink to="https://statusbadges.me" target="_blank" class="text-sky-400">statusbadges.me</NuxtLink>; Icons: <NuxtLink to="https://iconify.design" target="_blank" class="text-sky-400">iconify.design</NuxtLink></h6>
