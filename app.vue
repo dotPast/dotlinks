5fbf45fa3d2d51ea4cf842daa24023f07242136d
dotPast <83116565+dotPast@users.noreply.github.com>
2024-07-02 20:48:39 +0300

    attempt to fix margin for the status

--- app.vue
+++ app.vue
@@ -10,9 +10,9 @@
           <UButton to="/" icon="tabler:link" size="lg" color="black" variant="solid" label="/ links" />
           <UButton to="/about" icon="tabler:user" size="lg" color="black" variant="solid" label="/ about" />
           <UButton to="/what-i-worked-on" icon="tabler:hammer" size="lg" color="black" variant="solid" label="/ what i worked on" />
-        </UButtonGroup class="rounded mr-4">
+        </UButtonGroup class="rounded">
 
-        <img src="https://api.statusbadges.me/badge/status/585025499995045888?simple=true&style=flat-square" class="rounded"/>
+        <img src="https://api.statusbadges.me/badge/status/585025499995045888?simple=true&style=flat-square" class="rounded ml-4"/>
       </div>
       <NuxtPage class="mt-2"/>
       <h6 class="mt-4">Status Badge: <NuxtLink to="https://statusbadges.me" target="_blank" class="text-sky-400">statusbadges.me</NuxtLink>; Icons: <NuxtLink to="https://iconify.design" target="_blank" class="text-sky-400">iconify.design</NuxtLink></h6>
