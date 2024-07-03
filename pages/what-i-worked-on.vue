c770f1e865177517bce17b0606ed293d9e054475
dotPast <83116565+dotPast@users.noreply.github.com>
2024-07-03 16:02:48 +0300

    remove labels from what i worked on page

--- pages/what-i-worked-on.vue
+++ pages/what-i-worked-on.vue
@@ -5,23 +5,34 @@
 
       <h3>Streamers SMP</h3>
       <UButtonGroup>
-        <UButton to="https://streamerssmp.com/" target="_blank" icon="tabler:world" size="lg" color="white" variant="solid" label="Website" />
-        <UButton to="https://x.com/StreamersSMP__" target="_blank" icon="tabler:brand-twitter" size="lg" color="sky" variant="solid" label="Twitter / X" />
-        <UButton to="https://youtube.com/@StreamersSMP1" target="_blank" icon="tabler:brand-youtube" size="lg" color="red" variant="solid" label="YouTube" />
-        <UButton to="https://discord.gg/Mhe4btHW87" target="_blank" icon="tabler:brand-discord" size="lg" color="indigo" variant="solid" label="Discord" />
+        <UButton to="https://streamerssmp.com/" target="_blank" icon="tabler:world" size="lg" color="white" variant="solid" />
+        <UButton to="https://x.com/StreamersSMP__" target="_blank" icon="tabler:brand-twitter" size="lg" color="sky" variant="solid" />
+        <UButton to="https://youtube.com/@StreamersSMP1" target="_blank" icon="tabler:brand-youtube" size="lg" color="red" variant="solid" />
+        <UButton to="https://discord.gg/Mhe4btHW87" target="_blank" icon="tabler:brand-discord" size="lg" color="indigo" variant="solid" />
       </UButtonGroup>
 
       <h3>Dikoland (🇷🇺)</h3>
       <UButtonGroup>
-        <UButton to="https://dikoland.ru/" target="_blank" icon="tabler:world" size="lg" color="white" variant="solid" label="Website" />
-        <UButton to="https://discord.gg/apgCdEtu2w" target="_blank" icon="tabler:brand-discord" size="lg" color="indigo" variant="solid" label="Discord" />
+        <UButton to="https://dikoland.ru/" target="_blank" icon="tabler:world" size="lg" color="white" variant="solid" />
+        <UButton to="https://discord.gg/apgCdEtu2w" target="_blank" icon="tabler:brand-discord" size="lg" color="indigo" variant="solid" />
       </UButtonGroup>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+useSeoMeta({
+  title: 'dot\'s corner / what i worked on',
+  ogTitle: 'dot\'s corner',
+  description: 'dotPast\'s links website',
+  ogDescription: 'dotPast\'s links website',
+  ogImage: '/ogimage.png',
+  twitterCard: 'summary_large_image',
+  ogUrl: 'https://dottt.pages.dev',
+  twitterTitle: 'dot\'s corner',
+  twitterDescription: 'dotPast\'s links website',
+  twitterImage: '/ogimage.png'
+})
 </script>
 
 <style></style>
