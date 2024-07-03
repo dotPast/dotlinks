47c31fc3dadb7b4db5b91c6d2238fd560358da47
dotPast <83116565+dotPast@users.noreply.github.com>
2024-07-03 10:25:40 +0300

    change how the card is centered

--- app.vue
+++ app.vue
@@ -1,32 +1,31 @@
 <template>
-  <div>
-    <UCard class="center">
+  <UContainer class="center">
+    <UCard>
       <div class="hstack">
         <NuxtLink to="/">
           <img src="https://avatars.githubusercontent.com/u/83116565" width="48" class="rounded-xl mr-4"/>
         </NuxtLink>
 
-        <UButtonGroup>
+        <UButtonGroup class="mr-4">
           <UButton to="/" icon="tabler:link" size="lg" color="black" variant="solid" label="/ links" />
           <UButton to="/about" icon="tabler:user" size="lg" color="black" variant="solid" label="/ about" />
           <UButton to="/what-i-worked-on" icon="tabler:hammer" size="lg" color="black" variant="solid" label="/ what i worked on" />
-        </UButtonGroup class="rounded">
+        </UButtonGroup>
 
-        <img src="https://api.statusbadges.me/badge/status/585025499995045888?simple=true&style=flat-square" class="rounded ml-4"/>
+        <img src="https://api.statusbadges.me/badge/status/585025499995045888?simple=true&style=flat-square" class="rounded"/>
       </div>
       <NuxtPage class="mt-2"/>
       <h6 class="mt-4">Status Badge: <NuxtLink to="https://statusbadges.me" target="_blank" class="text-sky-400">statusbadges.me</NuxtLink>; Icons: <NuxtLink to="https://iconify.design" target="_blank" class="text-sky-400">iconify.design</NuxtLink></h6>
     </UCard>
-  </div>
+  </UContainer>
 </template>
 
 <style>
 .center {
   position: absolute;
-  left: 50%;
   top: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
+  -webkit-transform: translate(0%, -50%);
+  transform: translate(0%, -50%);
 }
 
 .hstack {
